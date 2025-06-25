15cc205c08a0701aef8c1937ff3fa8ebd4a261f5
rickey johnson <rdjwinter2004@gmail.com>
2025-06-25 11:19:40 -0700

    created dashboard page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router'
 import LandingPage from './pages/LandingPage'
 import SignUpPage from './pages/SignUpPage'
 import LoginPage from './pages/LoginPage'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
 	return (
@@ -9,6 +10,7 @@ const App = () => {
 			<Route path="/" element={<LandingPage />} />
 			<Route path="/signup" element={<SignUpPage />} />
 			<Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
 		</Routes>
 	)
 }
