ca3b6a3466ac804e2ccdd5480c5ae0771b51a09a
rickey johnson <rdjwinter2004@gmail.com>
2025-06-27 13:31:38 -0700

    designed link bank screen with back button and link bank button

--- client/src/pages/LinkPlaid.tsx
+++ client/src/pages/LinkPlaid.tsx
@@ -1,53 +1,71 @@
-import { usePlaidLink } from "react-plaid-link"
-import { useCallback, useEffect, useState } from "react"
-import { Button } from "../components/Button"
+import { usePlaidLink } from 'react-plaid-link'
+import { useCallback, useEffect, useState } from 'react'
+import { Button } from '../components/Button'
+import { Link } from 'react-router'
 
 const LinkPlaid = () => {
-  const [linkToken, setLinkToken] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
+	const [linkToken, setLinkToken] = useState(null)
+	const [isLoading, setIsLoading] = useState(false)
 
-  const generateToken = async () => {
-    console.log('generating link_token')
-    const response = await fetch('/api/plaid/create_link_token', {
-      method: 'POST',
-    })
+	const generateToken = async () => {
+		console.log('generating link_token')
+		const response = await fetch('/api/plaid/create_link_token', {
+			method: 'POST',
+		})
 
-    const data = await response.json()
-    setLinkToken(data.link_token)
-  }
+		const data = await response.json()
+		setLinkToken(data.link_token)
+	}
 
-  const onSuccess = useCallback((public_token: string) => {
-    try {
-      fetch('/api/plaid/set_access_token', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ public_token }),
-      })
+	const onSuccess = useCallback((public_token: string) => {
+		try {
+			fetch('/api/plaid/set_access_token', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ public_token }),
+			})
 
-      setIsLoading(false)
-    } catch(error: any) {
-      console.log(error.message)
-    }
+			setIsLoading(false)
+		} catch (error: any) {
+			console.log(error.message)
+		}
 
-  }, [])
+		setIsLoading(false)
+	}, [])
 
-  const config = {
-    token: linkToken,
-    onSuccess,
-  }
+	const config = {
+		token: linkToken,
+		onSuccess,
+	}
 
-  const { open, ready } = usePlaidLink(config)
+	const { open, ready } = usePlaidLink(config)
 
-  useEffect(() => {
-    setIsLoading(true)
-    generateToken()
-  }, [])
+	useEffect(() => {
+		setIsLoading(true)
+		generateToken()
+		setIsLoading(false)
+	}, [])
 
-  return (
-    <div>{isLoading ? 'Linking Plaid...' : 'Plaid Linked'}
-      <Button onClick={() => open()} disabled={!ready}>Link Plaid</Button>
-    </div>
-  )
+	return (
+		<div className="flex h-screen w-screen justify-center items-center relative">
+      <Link to='/signup' className='absolute left-5 top-5'>
+        <Button variant='ghost' className='flex justify-center items-center'>
+          <span className="material-icons">
+            arrow_back
+          </span>
+        </Button>
+      </Link>
+      <Button
+        onClick={() => {
+          setIsLoading(true)
+          open()
+        }}
+        disabled={!ready}
+      >
+        Link Bank
+      </Button>
+		</div>
+	)
 }
 
 export default LinkPlaid
