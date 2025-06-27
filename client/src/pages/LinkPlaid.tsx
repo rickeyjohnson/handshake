fe583a60f468b85655e1cc3ecf470702ea041f4f
rickey johnson <rdjwinter2004@gmail.com>
2025-06-27 11:32:51 -0700

    generated link token from backend on page load

--- client/src/pages/LinkPlaid.tsx
+++ client/src/pages/LinkPlaid.tsx
@@ -1,7 +1,24 @@
 import { Link } from "react-router"
 import { Button } from "../components/Button"
+import { usePlaidLink } from "react-plaid-link"
+import { useEffect, useState } from "react"
 
 const LinkPlaid = () => {
+  const [linkToken, setLinkToken] = useState(null)
+
+  const generateToken = async () => {
+    const response = await fetch('/api/plaid/create_link_token', {
+      method: 'POST',
+    })
+
+    const data = await response.json()
+    setLinkToken(data.link_token)
+  }
+
+  useEffect(() => {
+    generateToken()
+  }, [])
+
   return (
     <div>Linking Plaid...
 
