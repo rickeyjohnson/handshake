19e14c360ab00172ba73ac05da2c3044c9bb74b2
rickey johnson <rdjwinter2004@gmail.com>
2025-06-27 11:57:20 -0700

    successfuly implemented plaid's link to connect users with bank

--- client/src/pages/LinkPlaid.tsx
+++ client/src/pages/LinkPlaid.tsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router"
-import { Button } from "../components/Button"
 import { usePlaidLink } from "react-plaid-link"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { Button } from "../components/Button"
 
 const LinkPlaid = () => {
   const [linkToken, setLinkToken] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   const generateToken = async () => {
+    console.log('generating link_token')
     const response = await fetch('/api/plaid/create_link_token', {
       method: 'POST',
     })
@@ -15,16 +16,35 @@ const LinkPlaid = () => {
     setLinkToken(data.link_token)
   }
 
+  const onSuccess = useCallback((public_token: string) => {
+    try {
+      const response = fetch('/api/plaid/set_access_token', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ public_token }),
+      })
+
+    } catch(error: any) {
+      console.log(error.message)
+    }
+
+  }, [])
+
+  const config = {
+    token: linkToken,
+    onSuccess,
+  }
+
+  const { open, ready } = usePlaidLink(config)
+
   useEffect(() => {
+    setIsLoading(true)
     generateToken()
   }, [])
 
   return (
-    <div>Linking Plaid...
-
-        <Link to='/dashboard'>
-            <Button>to dashboard</Button>
-        </Link>
+    <div>{isLoading ? 'Linking Plaid...' : 'Plaid Linked'}
+      <Button onClick={() => open()} disabled={!ready}>Link Plaid</Button>
     </div>
   )
 }
