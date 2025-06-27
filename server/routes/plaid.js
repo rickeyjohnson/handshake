a9df42000c0c7417e7d14ed2110f14601af7b3f7
rickey johnson <rdjwinter2004@gmail.com>
2025-06-26 22:30:09 -0700

    added configuration as well as env client and secret variables

--- server/routes/plaid.js
+++ server/routes/plaid.js
@@ -1,5 +1,18 @@
 const plaid = require('express').Router()
 const { PrismaClient } = require('../generated/prisma')
 const prisma = new PrismaClient()
+const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
+const CLIENT_ID = process.env.PLAID_CLIENT_ID
+const SECRET = process.env.PLAID_SECRET
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': CLIENT_ID,
+      'PLAID-SECRET': SECRET,
+    },
+  },
+});
 
 module.exports = plaid
