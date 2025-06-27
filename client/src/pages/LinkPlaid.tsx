bf86746d5b18d5836e3aa57d825bfdce39f8cc4c
rickey johnson <rdjwinter2004@gmail.com>
2025-06-27 13:52:24 -0700

    formatted code

--- client/src/pages/LinkPlaid.tsx
+++ client/src/pages/LinkPlaid.tsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router'
 
 const LinkPlaid = () => {
 	const [linkToken, setLinkToken] = useState(null)
-	const [isLoading, setIsLoading] = useState(false)
 
 	const generateToken = async () => {
 		console.log('generating link_token')
@@ -24,13 +23,9 @@ const LinkPlaid = () => {
 				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify({ public_token }),
 			})
-
-			setIsLoading(false)
 		} catch (error: any) {
 			console.log(error.message)
 		}
-
-		setIsLoading(false)
 	}, [])
 
 	const config = {
@@ -41,31 +36,26 @@ const LinkPlaid = () => {
 	const { open, ready } = usePlaidLink(config)
 
 	useEffect(() => {
-		setIsLoading(true)
 		generateToken()
-		setIsLoading(false)
 	}, [])
 
 	return (
 		<div className="flex flex-col gap-5 h-screen w-screen justify-center items-center relative">
-      <Link to='/signup' className='absolute left-5 top-5'>
-        <Button variant='ghost' className='flex justify-center items-center'>
-          <span className="material-icons">
-            arrow_back
-          </span>
-        </Button>
-      </Link>
+			<Link to="/signup" className="absolute left-5 top-5">
+				<Button
+					variant="ghost"
+					className="flex justify-center items-center"
+				>
+					<span className="material-icons">arrow_back</span>
+				</Button>
+			</Link>
 
-      <h1 className='text-center text-3xl'>Connect bank via Plaid to continue creating your account.</h1>
-      <Button
-        onClick={() => {
-          setIsLoading(true)
-          open()
-        }}
-        disabled={!ready}
-      >
-        Connect Bank
-      </Button>
+			<h1 className="text-center text-3xl">
+				Connect bank via Plaid to continue creating your account.
+			</h1>
+			<Button onClick={() => open()} disabled={!ready}>
+				Connect Bank
+			</Button>
 		</div>
 	)
 }
