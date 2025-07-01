362707fbf34f427d76be19217d2519c4c1123c63
rickey johnson <rdjwinter2004@gmail.com>
2025-07-01 12:24:51 -0700

    converted pair from js to ts

--- server/routes/pair.ts
+++ server/routes/pair.ts
@@ -1,34 +1,46 @@
-const pair = require('express').Router()
-const { PrismaClient } = require('../generated/prisma')
-const { generateHandshakeCode, isAuthenticated, isExpired } = require('../utils/util')
+import { Router, Request, Response } from 'express'
+import { PrismaClient } from '../generated/prisma'
+import {
+	generateHandshakeCode,
+	isAuthenticated,
+	isExpired,
+} from '../utils/util'
+
+const pair = Router()
 const prisma = new PrismaClient()
 
-const EXPIRATION_DURATION = 60 * 10           // seconds
+const EXPIRATION_DURATION = 60 * 10 // seconds
 
-pair.get('/request', isAuthenticated, async (req, res) => {
-    const userId = req.session.user.id
+pair.get('/request', isAuthenticated, async (req: Request, res: Response) => {
+	const userId = req.session.user.id
 
-    const codeRequest = await prisma.pairRequest.findFirst({
-        where: { initiatorUserId: userId },
-        orderBy: {
-            createdAt: "desc"
-        }
-    })
+	const codeRequest = await prisma.pairRequest.findFirst({
+		where: { initiatorUserId: userId },
+		orderBy: {
+			createdAt: 'desc',
+		},
+	})
 
-    if (codeRequest && !isExpired(codeRequest.createdAt, EXPIRATION_DURATION)) {
-        res.status(200).json({ message: 'You already initiated a pair request', ...codeRequest })
-        return
-    }
+	if (codeRequest && !isExpired(codeRequest.createdAt, EXPIRATION_DURATION)) {
+		res.status(200).json({
+			message: 'You already initiated a pair request',
+			...codeRequest,
+		})
+		return
+	}
 
-    const code = generateHandshakeCode()
-    const pairRequest = await prisma.pairRequest.create({
-        data: {
-            code: code,
-            initiatorUserId: userId,
-        }
-    })
+	const code = generateHandshakeCode()
+	const pairRequest = await prisma.pairRequest.create({
+		data: {
+			code: code,
+			initiatorUserId: userId,
+		},
+	})
 
-    res.status(200).json({ message : 'You initiated a pair request', ...pairRequest })
+	res.status(200).json({
+		message: 'You initiated a pair request',
+		...pairRequest,
+	})
 })
 
-module.exports = pair
+export default pair
