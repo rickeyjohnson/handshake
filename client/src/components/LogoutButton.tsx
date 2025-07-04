9e79ca4dcd666de276f9b62188774ab63c5dd8d2
rickey johnson <rdjwinter2004@gmail.com>
2025-07-03 21:20:34 -0700

    replaced ccurrent state with initial state when use logs out

--- client/src/components/LogoutButton.tsx
+++ client/src/components/LogoutButton.tsx
@@ -1,8 +1,10 @@
+import { useAccount } from '../contexts/AccountContext'
 import { useUser } from '../contexts/UserContext'
 import { Button } from './Button'
 
 export const LogoutButton = () => {
 	const { setUser } = useUser()
+	const { setAccounts } = useAccount()
 
 	const handleLogout = async () => {
 		await fetch('/api/auth/logout', {
@@ -11,6 +13,19 @@ export const LogoutButton = () => {
 		})
 
 		setUser(null)
+		setAccounts([{
+			id: -1,
+			account_name: '',
+			bank_name: '',
+			user_id: '',
+			balances: {
+				available: 0,
+				current: 0,
+				currency_code: '',
+			},
+			subtype: '',
+			type: '',
+		}])
 	}
 
 	return <Button onClick={handleLogout}>Log Out</Button>
