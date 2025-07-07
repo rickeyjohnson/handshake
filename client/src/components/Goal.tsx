193420ac205939449f311b161c53a13073f7550a
rickey johnson <rdjwinter2004@gmail.com>
2025-07-07 16:18:22 -0700

    formatted code

--- client/src/components/Goal.tsx
+++ client/src/components/Goal.tsx
@@ -7,10 +7,7 @@ const Goal = ({
 	current: number
 	amount: number
 }) => {
-	// const width = () => {
-	//     return 'w-' +
-	// }
-
+	
 	return (
 		<div className="bg-slate-800 h-55 w-sm relative shrink rounded-xl bg-[url(https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&h=1000&q=80)] bg-center bg-cover bg-no-repeat">
 			<div className="absolute inset-0 bg-gradient-to-b from-transparent to-black to-70% flex flex-col justify-end p-4 text-white rounded-xl">
