324d56eccc458c550ea406fb2dc112e7c3bdeeb5
rickey johnson <rdjwinter2004@gmail.com>
2025-07-07 13:19:13 -0700

    created post goals endpoint for new goals creation

--- server/routes/goals.ts
+++ server/routes/goals.ts
@@ -19,4 +19,34 @@ goals.get('/', async (req, res) => {
     }
 })
 
+goals.post('/', async (req, res) => {
+    try {
+        const userId = req.session.user.id
+        const goal: {
+            title: string
+            description: string
+            target: number
+            current: number
+            deadline: string
+
+        } = req.body
+
+        const newGoal = await prisma.goals.create({
+            data: {
+                user_id: userId,
+                pair_id: await getPairedId(userId),
+                title: goal.title,
+                description: goal.description,
+                target: goal.target,
+                current: goal.current,
+                deadline: new Date(goal.deadline),
+            }
+        })
+
+        res.status(201).json({message: 'New goal created'})
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
 export default goals
