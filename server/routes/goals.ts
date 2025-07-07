27b472a6754ffd3530163f0f40aee532d7a84332
rickey johnson <rdjwinter2004@gmail.com>
2025-07-07 10:38:33 -0700

    added goals get endpoint to get goals from db

--- server/routes/goals.ts
+++ server/routes/goals.ts
@@ -1,7 +1,22 @@
 import { Router, Request, Response } from 'express'
 import { PrismaClient } from '../generated/prisma'
+import { getPairedId } from '../utils/util'
 
 const goals = Router()
 const prisma = new PrismaClient()
+
+goals.get('/', async (req, res) => {
+    try {
+        const userId = req.session.user.id
+        const pairId = await getPairedId(userId)
+        const goals = await prisma.goals.findMany({
+            where: { pair_id: pairId }
+        })
+
+        res.status(200).json(goals)
+    } catch(error) {
+        res.status(500).json({ error: error.message })
+    }
+})
 
 export default goals
