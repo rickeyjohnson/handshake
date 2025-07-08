3ac8b9ea80006d101c3a4e9d0f0be2052fcae149
rickey johnson <rdjwinter2004@gmail.com>
2025-07-08 16:32:42 -0700

    started prisma call for get endpoint

--- server/routes/budgets.ts
+++ server/routes/budgets.ts
@@ -1,7 +1,20 @@
 import { Router } from 'express'
 import { PrismaClient } from '../generated/prisma'
+import { getPairedId } from '../utils/util'
 
 const budgets = Router()
 const prisma = new PrismaClient()
+
+budgets.get('/', async (req, res) => {
+	try {
+        const userId = req.session.id
+        const pairId = getPairedId(userId)
+        const budgets = await prisma.budgets.findMany({
+            where: { pair_id: pairId }
+        })
+	} catch (error) {
+		res.status(500).json({ error: error.message })
+	}
+})
 
 export default budgets
