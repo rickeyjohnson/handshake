32197460296418960fbfb4ffb49c79f45a1cb6bb
rickey johnson <rdjwinter2004@gmail.com>
2025-07-10 13:06:22 -0700

    added image of captured image with ractangle drawing for future ocr + rectangle implemention

--- client/src/components/PriceSelection.tsx
+++ client/src/components/PriceSelection.tsx
@@ -1,36 +1,53 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from 'react'
 
 // remove any before pushing
-const PriceSelection = ({ prices, image_url }: { prices: any, image_url: string }) => {
-    const canvasRef = useRef<HTMLCanvasElement>(null)
+const PriceSelection = ({
+	image_url,
+}: {
+	image_url: string
+}) => {
+	const canvasRef = useRef<HTMLCanvasElement>(null)
 
-    const displayProcessedImage = () => {
-        const canvas = canvasRef.current
-        const ctx = canvas?.getContext('2d')
+	const displayProcessedImage = () => {
+		const canvas = canvasRef.current
+		const ctx = canvas?.getContext('2d')
 
-        if (!canvas || !ctx) return
+		if (!canvas || !ctx) return
 
-        const image = new Image()
-        image.src = image_url
+		const image = new Image()
+		image.src = image_url
 
-        image.onload = () => {
-            canvas.width = image.width
-            canvas.height = image.height
+		image.onload = () => {
+			canvas.width = image.width
+			canvas.height = image.height
 
-            ctx.drawImage(image, 0, 0)
-        }
-    }
+			ctx.drawImage(image, 0, 0)
 
-    useEffect(() => {
-        displayProcessedImage()
-    }, [])
+			drawRectangle(ctx, 76, 46, 144 - 76, 62 - 46)
+		}
+	}
 
-	return <div>
-        <div>{prices.text}</div>
-        <canvas ref={canvasRef}/>
-        <br />
-        <pre>{JSON.stringify(prices, null, "\t")}</pre>
-    </div>
+	const drawRectangle = (
+		ctx: CanvasRenderingContext2D,
+		x: number,
+		y: number,
+		width: number,
+		height: number
+	) => {
+		ctx.strokeStyle = 'red'
+		ctx.lineWidth = 2
+		ctx.strokeRect(x, y, width, height)
+	}
+
+	useEffect(() => {
+		displayProcessedImage()
+	}, [])
+
+	return (
+		<div>
+			<canvas ref={canvasRef} />
+		</div>
+	)
 }
 
 export default PriceSelection
--- client/src/pages/AddExpensePage.tsx
+++ client/src/pages/AddExpensePage.tsx
@@ -3,7 +3,6 @@ import MainHeader from '../components/MainHeader'
 import ReceiptCapture from '../components/ReceiptCapture'
 import { useState } from 'react'
 import PriceSelection from '../components/PriceSelection'
-import { extractPricesFromImage } from '../ocr'
 
 const AddExpensePage = () => {
 	const [image, setImage] = useState<string | null>(null)
@@ -11,8 +10,6 @@ const AddExpensePage = () => {
 
 	const handleCapture = async (url: string) => {
 		setImage(url)
-		const priceResults = await extractPricesFromImage(url)
-		setPrices(priceResults)
 	}
 
 	return (
@@ -20,7 +17,7 @@ const AddExpensePage = () => {
 			<MainHeader title="Add New Expense" />
 
 			<ReceiptCapture onCapture={handleCapture} />
-			{image && <PriceSelection prices={prices} image_url={image} />}
+			{image && <PriceSelection image_url={image} />}
 		</MainLayout>
 	)
 }
