00db516c6bf87b831ead6e1b4f66ce416f29fe7e
rickey johnson <rdjwinter2004@gmail.com>
2025-07-17 11:44:43 -0700

    added conditional success or notification conditional fif addition was made by user or pair

--- client/src/components/NotificationToast.tsx
+++ client/src/components/NotificationToast.tsx
@@ -1,18 +1,20 @@
 import { IconCircleCheck } from '@tabler/icons-react'
 import type { Notification } from '../types/types'
+import { useUser } from '../contexts/UserContext'
 
 const NotificationToast = ({ notification }: {notification: Notification}) => {
+  const { user } = useUser()
 	return (
 		<div className="flex justify-center col-span-full gap-2 rounded-2xl w-fit bg-white fixed right-3 top-3 py-3 px-8 border-1 border-stone-200">
 			<IconCircleCheck size={20} className="mt-0.5" />
-			<div>
+			{notification.user_id === user?.id ? <div>
 				<h1 className="font-medium text-md">
 					Success! Your chnages have been saved.
 				</h1>
 				<p className="font-light text-stone-400 text-sm">
 					Uyen will be notified of your changes.
 				</p>
-			</div>
+			</div> : <p>Todo</p>}
 		</div>
 	)
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,17 +7,16 @@ import { AccountProvider } from './contexts/AccountContext.tsx'
 import { TransactionProvider } from './contexts/TransactionsContext.tsx'
 import { WebSocketProvider } from './contexts/WebsocketContext.tsx'
 
-
 createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
-		<WebSocketProvider>
-			<UserProvider>
+		<UserProvider>
+			<WebSocketProvider>
 				<AccountProvider>
 					<TransactionProvider>
 						<App />
 					</TransactionProvider>
 				</AccountProvider>
-			</UserProvider>
-		</WebSocketProvider>
+			</WebSocketProvider>
+		</UserProvider>
 	</BrowserRouter>
 )
