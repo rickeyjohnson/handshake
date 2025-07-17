12717ae13a257698c8e4952e2290701c69971794
rickey johnson <rdjwinter2004@gmail.com>
2025-07-17 10:37:40 -0700

    formatted code

--- client/src/components/NotificationToast.tsx
+++ client/src/components/NotificationToast.tsx
@@ -1,15 +1,19 @@
-import { IconCircleCheck } from "@tabler/icons-react"
+import { IconCircleCheck } from '@tabler/icons-react'
 
 const NotificationToast = () => {
-  return (
-    <div className="flex justify-center col-span-full gap-2 rounded-2xl w-fit bg-white fixed right-3 top-3 py-3 px-8 border-1 border-stone-200">
-      <IconCircleCheck size={20} className="mt-0.5"/>
-      <div>
-        <h1 className="font-medium text-md">Success! Your chnages have been saved.</h1>
-        <p className="font-light text-stone-400 text-sm">Uyen will be notified of your changes.</p>
-      </div>
-    </div>
-  )
+	return (
+		<div className="flex justify-center col-span-full gap-2 rounded-2xl w-fit bg-white fixed right-3 top-3 py-3 px-8 border-1 border-stone-200">
+			<IconCircleCheck size={20} className="mt-0.5" />
+			<div>
+				<h1 className="font-medium text-md">
+					Success! Your chnages have been saved.
+				</h1>
+				<p className="font-light text-stone-400 text-sm">
+					Uyen will be notified of your changes.
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default NotificationToast
