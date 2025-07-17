1df4962c3f3bfe93e8d930c27ffeb0cf1d3af8f9
rickey johnson <rdjwinter2004@gmail.com>
2025-07-17 15:37:36 -0700

    added networth styling

--- client/src/components/NetWorth.tsx
+++ client/src/components/NetWorth.tsx
@@ -5,12 +5,14 @@ const NetWorth = ({networth, userNetworth, partnerNetworth} : {networth: number,
   const { user } = useUser()
 
   return (
-    <div className='rounded-lg border-1 border-stone-200 p-4 w-fit'>
-      <h1 className='mb-2'>Total Net Worth</h1>
-      <p className='text-5xl font-medium'>{formatCurrency(networth, true)}</p>
+    <div className='rounded-xl border-1 border-stone-200 py-6 px-8 w-fit flex flex-col'>
+      <h1 className='mb-2 p-1'>Total Net Worth</h1>
+      <p className='text-5xl font-medium mb-3 p-1'>{formatCurrency(networth, true)}</p>
 
-      <p className='capitalize'>{user?.name}'s net worth: {formatCurrency(userNetworth, true)}</p>
-      <p className='capitalize'>{user?.partner.name}'s net worth: {formatCurrency(partnerNetworth, true)}</p>
+      <div className='p-1'>
+        <p className='capitalize'>{user?.name}'s net worth: {formatCurrency(userNetworth, true)}</p>
+        <p className='capitalize'>{user?.partner.name}'s net worth: {formatCurrency(partnerNetworth, true)}</p>
+      </div>
     </div>
   )
 }
