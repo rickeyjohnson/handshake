f9a5cdd9c39381018cf533cd6a49b49bbab79cc0
rickey johnson <rdjwinter2004@gmail.com>
2025-07-17 15:40:39 -0700

    added styling for individual net worth aligning it with edges of container

--- client/src/components/NetWorth.tsx
+++ client/src/components/NetWorth.tsx
@@ -9,9 +9,15 @@ const NetWorth = ({networth, userNetworth, partnerNetworth} : {networth: number,
       <h1 className='mb-2 p-1'>Total Net Worth</h1>
       <p className='text-5xl font-medium mb-3 p-1'>{formatCurrency(networth, true)}</p>
 
-      <div className='p-1'>
-        <p className='capitalize'>{user?.name}'s net worth: {formatCurrency(userNetworth, true)}</p>
-        <p className='capitalize'>{user?.partner.name}'s net worth: {formatCurrency(partnerNetworth, true)}</p>
+      <div className='p-1 w-2xs'>
+        <div className='flex'>
+          <p className='capitalize flex-1'>Your net worth:</p>
+          <span className='font-medium'>{formatCurrency(userNetworth, true)}</span>
+        </div>
+        <div className='flex'>
+          <p className='capitalize flex-1'>{user?.partner.name}'s net worth:</p>
+          <span className='font-medium'>{formatCurrency(partnerNetworth, true)}</span>
+        </div>
       </div>
     </div>
   )
