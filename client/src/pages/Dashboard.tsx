41ec1b1d3c56be73ebb88fdd31f621ce4b24dc1c
rickey johnson <rdjwinter2004@gmail.com>
2025-07-17 15:04:34 -0700

    added components for future implementation to dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,15 +1,29 @@
+import Accounts from '../components/Accounts'
+import CalendarSummary from '../components/CalendarSummary'
 import MainHeader from '../components/layout/MainHeader'
 import MainLayout from '../components/layout/MainLayout'
 import NetWorth from '../components/NetWorth'
+import Spending from '../components/Spending'
+import SpendingBudgetGraph from '../components/SpendingBudgetGraph'
+import Transactions from '../components/Transactions'
+import { useAccount } from '../contexts/AccountContext'
+import { useTransactions } from '../contexts/TransactionsContext'
 import { useUser } from '../contexts/UserContext'
 
 const Dashboard = () => {
 	const { user } = useUser()
+	const { accounts } = useAccount()
+	const { transactions } = useTransactions()
 	
 	return (
 	<MainLayout>
 		<MainHeader title={`Welcome ${user?.name || 'Partner'}`} caption={`Today is ${new Date().toDateString()}`} />
 		<NetWorth />
+		<Spending />
+		<SpendingBudgetGraph />
+		<Transactions />
+		<Accounts />
+		<CalendarSummary />
 	</MainLayout>
 	)
 }
