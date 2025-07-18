98519ddbab93b66513048ab052f0a640bb8bf66c
rickey johnson <rdjwinter2004@gmail.com>
2025-07-18 12:22:57 -0700

    removed cols from transations dashboard component and started accounts component

--- client/src/components/Accounts.tsx
+++ client/src/components/Accounts.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { IconBuildingBank } from "@tabler/icons-react"
+import { useAccount } from "../contexts/AccountContext"
 
 const Accounts = () => {
+  const {accounts} = useAccount()
   return (
-    <div>Accounts</div>
+    <div className="rounded-xl border-1 border-stone-200 py-6 px-8 w-fit flex flex-col">
+      <div className="flex gap-2 flex-row align-center">
+        <IconBuildingBank size={20}/>
+        <h1 className="self-center">Checking</h1>
+        <p>${`${accounts}`}</p>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Transactions.tsx
+++ client/src/components/Transactions.tsx
@@ -8,17 +8,14 @@ const Transactions = () => {
 			<table className="bg-white flex-3 border-1 border-stone-200">
 				<thead>
 					<tr className="text-left bg-stone-100">
+            <th className="text-lg font-medium w-3xs px-3 py-2">
+							Date
+						</th>
 						<th className="text-lg font-medium w-md px-3 py-2">
 							Name
 						</th>
-						<th className="text-lg font-medium w-3xs px-3 py-2">
-							Date
-						</th>
 						<th className="text-lg font-medium w-2xs px-3 py-2">
 							User
-						</th>
-						<th className="text-lg font-medium w-sm px-3 py-2">
-							Account
 						</th>
 						<th className="text-lg font-medium pr-3 px-3 py-2">
 							Amount
@@ -32,16 +29,15 @@ const Transactions = () => {
 								key={tx.id}
 								className="border-1 border-stone-200"
 							>
-								<td className="px-3 py-2">
-									{tx.transaction_name}
+                <td className="px-3 py-2">
+									{tx.authorized_date || tx.date}
 								</td>
 								<td className="px-3 py-2">
-									{tx.authorized_date || tx.date}
+									{tx.transaction_name}
 								</td>
 								<td className="px-3 py-2 capitalize">
 									{tx.user_name}
 								</td>
-								<td className="px-3 py-2">{tx.account_name}</td>
 								<td className="text-right pr-3 py-2">
 									{formatCurrency(tx.amount)}
 								</td>
