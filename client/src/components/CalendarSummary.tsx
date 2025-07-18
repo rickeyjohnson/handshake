69df4c8742286bd364213083a35f4fffeba53b3b
rickey johnson <rdjwinter2004@gmail.com>
2025-07-18 15:20:51 -0700

    added calendar summary day headers for calender

--- client/src/components/CalendarSummary.tsx
+++ client/src/components/CalendarSummary.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
-
 const CalendarSummary = () => {
   return (
-    <div>CalendarSummary</div>
+    <div className="max-w-xl rounded-xl border-1 border-stone-200">
+      <div className="grid grid-cols-7 text-center">
+        {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day) => (
+          <div key={day}>{day}</div>
+        ))}
+      </div>
+    </div>
   )
 }
 
