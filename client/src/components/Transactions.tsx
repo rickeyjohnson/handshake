d6962f7b4bdfdfa4d604926609e4b4028321f11f
rickey johnson <rdjwinter2004@gmail.com>
2025-07-18 16:53:51 -0700

    rounded corner on taransactions table

--- client/src/components/Transactions.tsx
+++ client/src/components/Transactions.tsx
@@ -6,46 +6,51 @@ const Transactions = () => {
 	return (
 		<div>
 			<h1 className="py-2">Transactions</h1>
-			<div className="rounded-lg">
-				<table className="bg-white flex-3 border-1 border-stone-200">
+			<div className="overflow-hidden rounded-xl border border-stone-200">
+				<table className="bg-white w-full rounded-xl overflow-hidden">
 					<thead>
 						<tr className="text-left bg-stone-100">
-							<th className="text-lg font-medium w-3xs px-3 py-2">
+							<th className="text-lg font-medium w-xs pl-6 py-2">
 								Date
 							</th>
-							<th className="text-lg font-medium w-md px-3 py-2">
+							<th className="text-lg font-medium w-lg px-3 py-2">
 								Name
 							</th>
-							<th className="text-lg font-medium w-2xs px-3 py-2">
+							<th className="text-lg font-medium w-sm px-3 py-2">
 								User
 							</th>
-							<th className="text-lg font-medium pr-3 px-3 py-2">
+							<th className="text-lg font-medium w-sm px-3 py-2">
+								Account
+							</th>
+							<th className="text-lg font-medium pr-6 py-2 text-right">
 								Amount
 							</th>
 						</tr>
 					</thead>
+
 					<tbody>
-						{transactions.slice(0, 10).map((tx) => {
-							return (
-								<tr
-									key={tx.id}
-									className="border-1 border-stone-200"
-								>
-									<td className="px-3 py-2">
-										{tx.authorized_date || tx.date}
-									</td>
-									<td className="px-3 py-2">
-										{tx.transaction_name}
-									</td>
-									<td className="px-3 py-2 capitalize">
-										{tx.user_name}
-									</td>
-									<td className="text-right pr-3 py-2">
-										{formatCurrency(tx.amount)}
-									</td>
-								</tr>
-							)
-						})}
+						{transactions.slice(0, 10).map((tx) => (
+							<tr
+								key={tx.id}
+								className="border-t border-stone-200"
+							>
+								<td className="pl-6 py-2">
+									{tx.authorized_date || tx.date}
+								</td>
+								<td className="px-3 py-2">
+									{tx.transaction_name}
+								</td>
+								<td className="px-3 py-2 capitalize">
+									{tx.user_name}
+								</td>
+								<td className="px-3 py-2 capitalize">
+									{tx.account_name}
+								</td>
+								<td className="pr-6 py-2 text-right">
+									{formatCurrency(tx.amount)}
+								</td>
+							</tr>
+						))}
 					</tbody>
 				</table>
 			</div>
