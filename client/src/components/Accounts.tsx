e5295edde5732d240e38e2018d23b53655d9ce8a
rickey johnson <rdjwinter2004@gmail.com>
2025-07-18 12:26:33 -0700

    added map and accounts data state var for rendering organized account data based off checking, savings, etc.

--- client/src/components/Accounts.tsx
+++ client/src/components/Accounts.tsx
@@ -1,17 +1,22 @@
-import { IconBuildingBank } from "@tabler/icons-react"
-import { useAccount } from "../contexts/AccountContext"
+import { IconBuildingBank } from '@tabler/icons-react'
+import { useAccount } from '../contexts/AccountContext'
+import { useState } from 'react'
 
 const Accounts = () => {
-  const {accounts} = useAccount()
-  return (
-    <div className="rounded-xl border-1 border-stone-200 py-6 px-8 w-fit flex flex-col">
-      <div className="flex gap-2 flex-row align-center">
-        <IconBuildingBank size={20}/>
-        <h1 className="self-center">Checking</h1>
-        <p>${`${accounts}`}</p>
-      </div>
-    </div>
-  )
+	const { accounts } = useAccount()
+	const [accountsData, setAccountsData] = useState([{}])
+
+	return (
+		<div className="rounded-xl border-1 border-stone-200 py-6 px-8 w-fit flex flex-col">
+			{accountsData.map((acc) => (
+				<div className="flex gap-2 flex-row align-center">
+					<IconBuildingBank size={20} />
+					<h1 className="self-center">Checking</h1>
+					<p>${`${acc}`}</p>
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default Accounts
