4c030d1747b2c7fa7d7c48dc88b1843897da89e1
rickey johnson <rdjwinter2004@gmail.com>
2025-07-18 15:47:37 -0700

    made current calender based ui component

--- client/src/components/CalendarSummary.tsx
+++ client/src/components/CalendarSummary.tsx
@@ -1,13 +1,63 @@
+import {
+	startOfMonth,
+	endOfMonth,
+	startOfWeek,
+	endOfWeek,
+	addDays,
+	isSameMonth,
+	format,
+} from 'date-fns'
+
 const CalendarSummary = () => {
-  return (
-    <div className="max-w-xl rounded-xl border-1 border-stone-200">
-      <div className="grid grid-cols-7 text-center">
-        {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day) => (
-          <div key={day}>{day}</div>
-        ))}
-      </div>
-    </div>
-  )
+	const today = new Date()
+	const monthStart = startOfMonth(today)
+	const monthEnd = endOfMonth(monthStart)
+	const calendarStart = startOfWeek(monthStart, { weekStartsOn: 0 }) // Sunday
+	const calendarEnd = endOfWeek(monthEnd, { weekStartsOn: 0 }) // Saturday
+
+	const days = []
+	let day = calendarStart
+	while (day <= calendarEnd) {
+		days.push(day)
+		day = addDays(day, 1)
+	}
+
+	return (
+		<div className="max-w-xl rounded-xl border-1 border-stone-200 p-4">
+			<h1 className="w-full flex justify-center p-4">
+				{new Date().toLocaleString('en-US', {
+					month: 'long',
+					year: 'numeric',
+				})}
+			</h1>
+			<div className="grid grid-cols-7 text-center my-4 text-stone-500 font-light">
+				{['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(
+					(day) => (
+						<div key={day}>{day}</div>
+					)
+				)}
+			</div>
+
+			<div className="grid grid-cols-7 gap-1">
+				{days.map((date, idx) => {
+					const isCurrentMonth = isSameMonth(date, monthStart)
+
+					return (
+						<div
+							key={idx}
+							className={`flex h-14 items-center justify-center rounded-md text-sm hover:bg-stone-50 hover:cursor-default bg-white ${
+								isCurrentMonth
+									? 'text-black'
+									: 'text-stone-400/75'
+							}`}
+						>
+							{format(date, 'd')}
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
 export default CalendarSummary
