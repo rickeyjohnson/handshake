8f02716078ca283eb877ad04bf0b73968c4b77fa
rickey johnson <rdjwinter2004@gmail.com>
2025-07-18 10:30:03 -0700

    formatted code

--- client/src/components/Spending.tsx
+++ client/src/components/Spending.tsx
@@ -2,16 +2,11 @@ import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
 import { formatCurrency } from '../utils/utils'
 import type { SpendingData } from '../types/types'
 
-const Spending = ({
-	total,
-	data,
-}: {
-	total: number
-	data: SpendingData[]
-}) => {
-  const xAxisDataPoints = 4
-  const formatYAxis = (value: number) => `$${value}`
-  const formatXAxis = (date: Date) => `${date.toLocaleDateString('en-US', {month: 'long', day: 'numeric'})}`
+const Spending = ({ total, data }: { total: number; data: SpendingData[] }) => {
+	const xAxisDataPoints = 4
+	const formatYAxis = (value: number) => `$${value}`
+	const formatXAxis = (date: Date) =>
+		`${date.toLocaleDateString('en-US', { month: 'long', day: 'numeric' })}`
 	return (
 		<div className="rounded-xl border-1 border-stone-200 py-6 px-8 w-fit flex flex-col">
 			<h1 className="mb-2 p-1">Total Month Spendning</h1>
@@ -19,11 +14,20 @@ const Spending = ({
 				{formatCurrency(Math.abs(total))}
 			</p>
 
-			<div className="w-100">
+			<div className="w-100 h-auto">
 				<ResponsiveContainer width="100%" height="100%">
 					<LineChart data={data}>
-						<YAxis dataKey="total" tickFormatter={formatYAxis} orientation='right'/>
-            <XAxis dataKey="date" padding={{left: 30}} tickFormatter={formatXAxis} interval={Math.round(data.length / xAxisDataPoints)}/>
+						<YAxis
+							dataKey="total"
+							tickFormatter={formatYAxis}
+							orientation="right"
+						/>
+						<XAxis
+							dataKey="date"
+							padding={{ left: 30 }}
+							tickFormatter={formatXAxis}
+							interval={Math.round(data.length / xAxisDataPoints)}
+						/>
 						<Line
 							type="monotone"
 							dataKey="total"
