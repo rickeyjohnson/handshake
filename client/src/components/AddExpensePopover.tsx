4441f03a85ae82a000611bfd0bd539c173804732
rickey johnson <rdjwinter2004@gmail.com>
2025-07-19 16:47:35 -0700

    removed enter expense manually option

--- client/src/components/AddExpensePopover.tsx
+++ client/src/components/AddExpensePopover.tsx
@@ -11,13 +11,6 @@ const AddExpensePopover = () => {
 	return (
 		<div className="flex flex-col items-start border-2 border-stone-200 rounded-lg p-1 px-2 absolute z-50 bg-white w-xs right-0 top-12">
 			<div className="w-full text-left p-1">
-				<Button variant="ghost" className="flex gap-2 w-full text-left">
-					<IconPencil />
-					Enter Expense Manually
-				</Button>
-			</div>
-
-			<div className="border-t-2 border-t-stone-200 w-full text-left p-1">
 				<Button
 					variant="ghost"
 					className="flex gap-2 w-full"
