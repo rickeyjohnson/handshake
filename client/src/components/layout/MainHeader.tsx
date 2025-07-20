d2d441ccb9463dc31f6cdf993eee3afd0473e435
rickey johnson <rdjwinter2004@gmail.com>
2025-07-19 19:16:15 -0700

    flex wrap header content so its more responsive on smaller screens

--- client/src/components/layout/MainHeader.tsx
+++ client/src/components/layout/MainHeader.tsx
@@ -10,7 +10,7 @@ const MainHeader = ({
 	children?: React.ReactNode
 }) => {
 	return (
-		<div className="flex w-full mb-4 relative">
+		<div className="flex flex-wrap w-full mb-4 relative gap-2">
 			<div className="grow">
 				<h1 className="font-medium text-3xl capitalize">{title}</h1>
 				<p className="text-gray-500 capitalize">{caption}</p>
