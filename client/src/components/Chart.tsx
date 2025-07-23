5fda8e6f35d4e4a91625f396a689c7630aae89c2
rickey johnson <rdjwinter2004@gmail.com>
2025-07-22 17:25:06 -0700

    added typing and parameters to the component

--- client/src/components/Chart.tsx
+++ client/src/components/Chart.tsx
@@ -1,26 +1,23 @@
-const Chart = () => {
-	const width = 600
-	const height = 400
+type SimpleLinePlotProps = {
+	data: any[]
+	width?: number
+	height?: number
+	margin: { top: number; right: number; bottom: number; left: number }
+}
 
-	const points = [
-		{ x: 50, y: 350 },
-		{ x: 150, y: 200 },
-		{ x: 250, y: 300 },
-		{ x: 350, y: 100 },
-		{ x: 450, y: 250 },
-		{ x: 550, y: 150 },
-	]
+const Chart: React.FC<SimpleLinePlotProps> = ({
+	data,
+	width = 600,
+	height = 400,
+	margin = { top: 20, right: 20, bottom: 30, left: 40 },
+}) => {
+	if (!data.length) return <p>No data to display</p>
 
-	const pathData = points
-		.map((p, i) => (i === 0 ? `M${p.x} ${p.y}` : `L${p.x} ${p.y}`))
-		.join(' ')
+	const chartWidth = width - margin.left - margin.right
+	const chartHeight = height - margin.top - margin.bottom
 
 	return (
-		<svg
-			width={width}
-			height={height}
-            className="border-1"
-		>
+		<svg width={width} height={height} className="border-1">
 			<path d={pathData} fill="none" stroke="black" strokeWidth={2} />
 		</svg>
 	)
