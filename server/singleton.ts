50256698756febe6377cd71dfb16a0f4d82d47fc
rickey johnson <rdjwinter2004@gmail.com>
2025-07-25 12:49:25 -0700

    removed mock client

--- server/singleton.ts
+++ server/singleton.ts
@@ -3,13 +3,4 @@ import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
 
 import prisma from './prismaClient'
 
-jest.mock('./client', () => ({
-  __esModule: true,
-  default: mockDeep<PrismaClient>(),
-}))
-
-beforeEach(() => {
-  // mockReset(prismaMock)
-})
-
 export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
