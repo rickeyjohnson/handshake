dfaafc987c76701c7484c40e1642a6dffbf75aae
rickey johnson <rdjwinter2004@gmail.com>
2025-07-25 13:24:29 -0700

    added test to test ocr node

--- client/src/utils/__tests__/ocr.test.ts
+++ client/src/utils/__tests__/ocr.test.ts
@@ -39,4 +39,30 @@ describe('ocr.ts test cases', () => {
 	it('returns original string if no $', () => {
 		expect(normalize('99.99')).toBe('99.99')
 	})
+
+    it('parses valid OCRNode data', () => {
+		const input: OCRNode[] = [
+			{
+				text: '$5.00',
+				bbox: {x0: 0, y0: 0, x1: 10, y1: 10},
+				symbols: [{}],
+			},
+			{
+				text: 'invalid',
+				bbox: {x0: 10, y0: 10, x1: 20, y1: 20},
+				symbols: [{}],
+			},
+			{
+				text: '12.34',
+				bbox: {x0: 30, y0: 30, x1: 40, y1: 40},
+				symbols: [{}],
+			},
+		]
+
+		const result = parseOCRData(input)
+		expect(result).toEqual([
+			{ text: '5.00', bbox: {x0: 0, y0: 0, x1: 10, y1: 10}, selected: false, hovered: false },
+			{ text: '12.34', bbox: {x0: 30, y0: 30, x1: 40, y1: 40}, selected: false, hovered: false },
+		])
+    })
 })
